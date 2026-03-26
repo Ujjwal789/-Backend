ac676bf3464639efcc65f54e6decb6ab9cadd334
Ujjwal789 <156940582+Ujjwal789@users.noreply.github.com>
2026-03-26 15:46:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/about', (req, res) => {
+  res.send('About Page')
+})
+app.get('/login',(req, res)=>{
+ res.send('<h1> Please Login')
+})
+
+
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
